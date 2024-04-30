aec7a1d9a12618915e4cad665f168259bb2d6e1e
Wira07 <wiralodrasaputra07@gmail.com>
2024-04-30 11:24:49 +0700

    Initial commit

---
+++ app/src/main/java/com/wira_fkom/praktikum_pab_modul5/Tab1.java
@@ -0,0 +1,13 @@
+package com.wira_fkom.praktikum_pab_modul5;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Tab1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tab1);
+    }
+}
